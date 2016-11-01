60b2e77e6b28e62704cecdf274b512be1db7cfa0
Ambre Thomas <embrynette@gmail.com>
2016-11-02 00:44:32 +0100

    ajout du header

---
+++ ex27/inclusions.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inclusions.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athomas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/21 09:46:33 by athomas           #+#    #+#             */
+/*   Updated: 2016/07/21 12:21:12 by athomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUSIONS_H
+# define INCLUSIONS_H
+
+# define BUF_SIZE 4096
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putchar_err(char c);
+void	ft_putstr_err(char *str);
+
+#endif
